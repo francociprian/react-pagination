f64e324d9cd9ee430906860b245d3a89d6ac228c
Franco Ciprian <franco_ciprian@hotmail.com>
2023-05-10 12:24:00 -0300

    Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Layout() {
     return (
-      <div className='h-screen w-screen flex justify-center items-center'>
+      <div className='h-screen w-screen flex flex-col items-center'>
+        <div className='my-24'>
+          <h1 className='text-5xl font-sans my-8 underline underline-offset-2'>React Pagination</h1>
+        </div>
         <nav>
           <ul>
             <li className='text-4xl font-sans my-8 hover:underline hover:underline-offset-4'>
