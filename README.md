c979de7884a5cd102254af9cea2f5131e3cb8353
Franco Ciprian <franco_ciprian@hotmail.com>
2023-05-10 11:42:06 -0300

    README.md

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # 🚀 React Pagination
 
-### Example of pagination using React and @tanstack/react-query
+### Example using React, Axios, useEffect and fetch()
+
+<p align="center">
+  <img width="700" src="./public/paginationDefault.png" alt="Pagination"/>
+</p>
+
+### Example using React and @tanstack/react-query
 
 <p align="center">
   <img width="700" src="./public/pagination.png" alt="Pagination"/>
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,11 +6,11 @@ function Layout() {
       <div className='h-screen w-screen flex justify-center items-center'>
         <nav>
           <ul>
-            <li className='text-6xl font-sans'>
-              <Link to="example-default">Example 1</Link>
+            <li className='text-4xl font-sans my-8 hover:underline hover:underline-offset-4'>
+              <Link to="example-default">Example using React, Axios, useEffect and fetch()</Link>
             </li>
-            <li className='text-6xl font-sans'>
-              <Link to="example-react-query">Example 2</Link>
+            <li className='text-4xl font-sans my-8 hover:underline hover:underline-offset-4'>
+              <Link to="example-react-query">Example using React and @tanstack/react-query</Link>
             </li>
           </ul>
         </nav>
